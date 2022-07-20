328ccc91202ed135f3bf40c9ac5b1a8290173b49
rahul <rahulslg20@gmail.com>
2022-07-20 14:05:42 +0530

    added cards for shows and actors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 
 function Home() {
   const [input, setInput] = useState('');
@@ -29,17 +31,16 @@ function Home() {
     setSearchOption(ev.target.value);
   };
 
-  console.log(searchOption);
   const renderResults = () => {
     if (results && results.length === 0) {
       return <div>No results</div>;
     }
     if (results && results.length > 0) {
-      return results[0].show ? results.map(item => (
-        <div key={item.show.id}>{item.show.name}</div>
-      )) : results.map(item => (
-        <div key={item.person.id}>{item.person.name}</div>
-      ))
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
