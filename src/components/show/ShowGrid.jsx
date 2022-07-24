1a9b87a1071bfb87c3639e3baf1f3d0a82fb53ea
rahul <rahulslg20@gmail.com>
2022-07-25 01:00:59 +0530

    optimized components

--- src/components/show/ShowGrid.jsx
+++ src/components/show/ShowGrid.jsx
@@ -10,17 +10,20 @@ import { useShows } from '../../misc/custom-hooks';
 function ShowGrid({ data }) {
   const [starredShows, dispatchStarred] = useShows();
 
+  const onStarClick = useCallback(
+    (showId, isStarred) => {
+      if (isStarred) {
+        dispatchStarred({ type: 'REMOVE', showId });
+      } else {
+        dispatchStarred({ type: 'ADD', showId });
+      }
+    },
+    [dispatchStarred]
+  );
+
   return (
     <FlexGrid>
       {data.map(({ show }) => {
-        const isStarred = starredShows.includes(show.id);
-        const onStarClick = useCallback(() => {
-          if (isStarred) {
-            dispatchStarred({ type: 'REMOVE', showId: show.id });
-          } else {
-            dispatchStarred({ type: 'ADD', showId: show.id });
-          }
-        }, [isStarred, show.id]);
         return (
           <ShowCard
             key={show.id}
@@ -29,7 +32,7 @@ function ShowGrid({ data }) {
             image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
             summary={show.summary}
             onStarClick={onStarClick}
-            isStarred={isStarred}
+            isStarred={starredShows.includes(show.id)}
           />
         );
       })}
