276ae9e43b36f069e5d62bc6b9971f213a6d2279
rahul <rahulslg20@gmail.com>
2022-08-26 19:26:11 +0530

    Updated subtitle

--- src/components/MainPageLayout.jsx
+++ src/components/MainPageLayout.jsx
@@ -5,7 +5,10 @@ import Title from './Title';
 function MainPageLayout({ children }) {
   return (
     <div>
-        <Title title = "Box Office" subtitle="Are you looking for a movie or an actor?" />
+      <Title
+        title="Box Office"
+        subtitle="Are you looking for a TV Show or an actor?"
+      />
       <Navs />
       {children}
     </div>
